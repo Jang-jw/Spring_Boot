30a6ad09290bbbd47241fdc3c65106853d6a076b
장재욱 <jangjw@jangjaeug-ui-MacBookAir.local>
2023-10-30 12:58:27 +0900

    프레임워크 설정 최적화 / 구동확인

--- SpringBoot01/src/main/java/com/smhrd/controller/TestController.java
+++ SpringBoot01/src/main/java/com/smhrd/controller/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-	@RequestMapping("/ex01")
+	@RequestMapping("/")
 	public String ex01(Model model, HttpSession session) {
 
 		model.addAttribute("name", "김준우");
